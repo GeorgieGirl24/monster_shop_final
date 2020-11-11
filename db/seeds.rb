d228cb812073ba29948beb017888c9e3cd9cab89
George Soderholm <georgesoderholm@gmail.com>
2020-11-11 09:47:50 -0700

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ metroid_shop = Merchant.create(name: "Hope's Metroid Shop", address: '125 XR42 S
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
 
 #dog_shop items
-pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", active: true, inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active:false, inventory: 21)
 
 #metroid_shop items
-missiles = metroid_shop.items.create(name: "Missiles", description: "Access to new areas!", price: 10, image: "https://i.ibb.co/HVNNc7V/item-missile.gif", inventory: 32)
-power_bombs = metroid_shop.items.create(name: "Power Bombs", description: "No one is safe!", price: 200, image: "https://i.ibb.co/1rH5cp5/pbomb-n-02.gif", inventory: 21)
-varia_suit = metroid_shop.items.create(name: "Varia Suit", description: "Get you through acid!", price: 500, image: "https://i.ibb.co/K6sQT2b/MP-VS.jpg", inventory: 32)
-super_missiles = metroid_shop.items.create(name: "Super Missiles", description: "The boss puncher!", price: 100, image: "https://i.ibb.co/Kbgndr2/SM.jpg", inventory: 21)
+missiles = metroid_shop.items.create(name: "Missiles", description: "Access to new areas!", price: 10, image: "https://i.ibb.co/HVNNc7V/item-missile.gif", active: true, inventory: 32)
+power_bombs = metroid_shop.items.create(name: "Power Bombs", description: "No one is safe!", price: 200, image: "https://i.ibb.co/1rH5cp5/pbomb-n-02.gif", active: true, inventory: 21)
+varia_suit = metroid_shop.items.create(name: "Varia Suit", description: "Get you through acid!", price: 500, image: "https://i.ibb.co/K6sQT2b/MP-VS.jpg", active: true, inventory: 32)
+super_missiles = metroid_shop.items.create(name: "Super Missiles", description: "The boss puncher!", price: 100, image: "https://i.ibb.co/Kbgndr2/SM.jpg", active: true, inventory: 21)
 
 #megan items
 orge = megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
